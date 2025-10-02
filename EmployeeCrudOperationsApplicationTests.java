b66897de880651d4474d48aaa7f6ad4d678be22c
srinivasulu <srinivasulupunamalli5@gmail.com>
2025-10-02 15:19:41 +0530

    Add files via upload

---
+++ EmployeeCrudOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeCrudOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
